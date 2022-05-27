4fb1bb69c332cc4a3aaebb1452d6880188823b60
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-27 16:41:28 -0700

    psuedo code for ts file

---
+++ script.ts
@@ -0,0 +1,11 @@
+// Book Class: will be used to create a book object
+
+//UI Class: will be used to create a UI object
+
+// Store Class: will be used to create a store objects
+
+// Show books
+
+// Add books
+
+// remove books
