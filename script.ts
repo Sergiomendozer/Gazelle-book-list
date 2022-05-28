1b4217d4c9008cd6822985009c2d662f6d60fb77
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-27 19:33:59 -0700

    list of book made and table class made

--- script.ts
+++ script.ts
@@ -12,28 +12,40 @@ class book_elements {
 //table Class: will be used to create a table objects
 class table {
   static display_book_list() {
-    // const books: String = Store.get_books();
-    // // foreach is a loop that will run for each element in the array
-    // // books.ForEach((books) => table.add_book_to_table(books));
-    // books.forEach(function (book_elements: String) {
-    //   table.add_book_to_table(book_elements);
-    // });
+    // const books: String = Store.get_books(); use for later
+    const list_of_books: { title: String; author: String; isbn: String }[] = [
+      {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        isbn: "0-395-07749-X",
+      },
+      {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        isbn: "0-395-07749-X",
+      },
+    ];
   }
-
-  //   static add_book_to_table(book: String) {
-  //     const table: HTMLTableElement = document.getElementById(
-  //       "list"
-  //     )! as HTMLTableElement;
-  //     const row: HTMLTableRowElement = table.insertRow();
-  //     row.insertCell().innerHTML = book_elements.title;
-  //     row.insertCell().innerHTML = book_elements.author;
-  //     row.insertCell().innerHTML = book_elements.isbn;
-  //     const delete_button: HTMLButtonElement = document.createElement("button");
-  //     delete_button.innerHTML = "delete";
-  //     delete_button.setAttribute("class", "delete");
-  //     row.insertCell().appendChild(delete_button);
-  //   }
 }
+// // foreach is a loop that will run for each element in the array
+// // books.ForEach((books) => table.add_book_to_table(books));
+// books.forEach(function (book_elements: String) {
+//   table.add_book_to_table(book_elements);
+// });
+
+//   static add_book_to_table(book: String) {
+//     const table: HTMLTableElement = document.getElementById(
+//       "list"
+//     )! as HTMLTableElement;
+//     const row: HTMLTableRowElement = table.insertRow();
+//     row.insertCell().innerHTML = book_elements.title;
+//     row.insertCell().innerHTML = book_elements.author;
+//     row.insertCell().innerHTML = book_elements.isbn;
+//     const delete_button: HTMLButtonElement = document.createElement("button");
+//     delete_button.innerHTML = "delete";
+//     delete_button.setAttribute("class", "delete");
+//     row.insertCell().appendChild(delete_button);
+//   }
 
 // Store Class: will be used to create a store objects
 
